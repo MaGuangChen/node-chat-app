9554cee3d7be6bd3b779c421dda5e5704ba84857
Paul Ma <kwn791122@gmail.com>
2018-01-19 15:50:49 +0800

    add server and client websocket connect

--- server/server.js
+++ server/server.js
@@ -7,15 +7,32 @@ const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3000;
-var app = express();
+const app = express();
 
 // 平常是express在背景幫我們做掉了
 // 這次我們是把app帶入http模組作為callback function
 // 並且把建立的http server丟進socketIO 作為callback function
-var server = http.createServer(app);
-var io = socketIO(server);
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath)); // middleware 代表讓express host這個靜態檔案
+
+// 這邊的.on method第一個參數是事件
+// 第二個參數是一個callback function
+// websocket是一個persistent tec(持續科技)
+// 代表著server端跟client端可以持續的將communication channel開啟
+// 並且保持著這個雙向的資料通道直到其中一方不爽連為止
+// 如果是client端發起中斷連線，那server端也不能做甚麼，反之亦然
+// 但特殊例外的情況像是我們使用nodemon每次重啟server時
+// client端其實是會試圖重新取得連線的(reconnect)
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    // 這邊是socket.on聽某個事件的callback function
+    socket.on('disconnect', () => {
+      console.log('User was disconnected');
+    });
+});
+
 
 // 以往我們使用express 直接聽port 
 // 在這邊我們用http server去聽
